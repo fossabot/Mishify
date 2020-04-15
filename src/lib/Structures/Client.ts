8ea9591b15278fa7bac9d4e5469cd638d683b483
unknown <phamzito@hotmail.com>
2020-04-15 15:17:00 -0500

    iniciando bot

---
+++ src/lib/Structures/Client.ts
@@ -0,0 +1,10 @@
+import { Client, Collection, ClientOptions } from "discord.js";
+
+export default class MishifyClient extends Client {
+    public events: Collection<string, any>
+
+    constructor(options: ClientOptions) {
+        super(options);
+        this.events = new Collection<string, any>();
+    }
+}
