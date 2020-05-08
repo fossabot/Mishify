d8f50b58bc2926bf3dd27337fa04cca6d72598dc
Phamzito <phamzito@hotmail.com>
2020-05-07 21:12:19 -0500

    se añadio la configuracion requerida

--- src/Mishify.ts
+++ src/Mishify.ts
@@ -1,10 +1,43 @@
 import { Constants } from 'discord.js';
 import { Mishify } from './lib';
-/* eslint-disable */
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 // @ts-ignore
 Constants.DefaultOptions.ws.properties.$browser = 'Discord Android';
-/* eslint-enable */
 
-const client: Mishify = new Mishify();
+/* eslint-disable no-process-env */
+const client: Mishify = new Mishify({
+	prefix: process.env.PREFIX,
+	language: process.env.LANGUAGE,
+	commandEditing: true,
+	providers: {
+		mongodb: {
+			db: 'mishify',
+			connectionString: process.env.MONGO_URL,
+			options: {}
+		},
+		default: 'mongodb'
+	},
+	preserveSettings: false,
+	console: {
+		timestamps: true,
+		utc: false,
+		colors: {
+			debug: { time: { background: 'magenta' } },
+			error: { time: { background: 'red' } },
+			log: { time: { background: 'blue' } },
+			verbose: { time: { text: 'gray' } },
+			warn: { time: { background: 'lightyellow', text: 'black' } },
+			wtf: { message: { text: 'red' }, time: { background: 'red' } }
+		}
+	},
+	production: false,
+	prefixCaseInsensitive: true,
+	messageSweepInterval: 480,
+	messageCacheLifetime: 120,
+
+	commandMessageLifetime: 120,
+	owners: ['497061687820812288'],
+	aliasFunctions: { returnMethod: 'run', prefix: 'funcs', enabled: true }
+});
 /* eslint-disable no-process-env */
 client.login(process.env.TOKEN);
