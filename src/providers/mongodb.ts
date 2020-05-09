2b7a81ddaae718fae081667c1d02f32f8560684e
Phamzito <phamzito@hotmail.com>
2020-05-09 01:15:42 -0500

    isObject is not defined arreglado

--- src/providers/mongodb.ts
+++ src/providers/mongodb.ts
@@ -1,10 +1,9 @@
-/* eslint-disable */
 import { Provider, ProviderStore, ProviderOptions, util } from 'klasa';
 import { MongoClient } from 'mongodb';
 
 export default class extends Provider {
 
-	public db: null | MongoClient
+	public db: null | any
 	constructor(store: ProviderStore, file: string[], directory: string, options?: ProviderOptions) {
 		super(store, file, directory, options);
 		this.db = null;
@@ -24,6 +23,7 @@ export default class extends Provider {
 			connectionString,
 			util.mergeObjects(connection.options, { useNewUrlParser: true, useUnifiedTopology: true })
 		);
+		// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 		// @ts-ignore
 		this.db = mongoClient.db(connection.db);
 	}
@@ -34,72 +34,61 @@ export default class extends Provider {
 	}
 
 	public hasTable(table: string): Promise<boolean> {
-		// @ts-ignore
 		return this.db.listCollections().toArray().then(collections => collections.some(col => col.name === table));
 	}
 
 	public createTable(table: string): Promise<any> {
-		// @ts-ignore
 		return this.db.createCollection(table);
 	}
 
 	public deleteTable(table: string): Promise<any> {
-		// @ts-ignore
 		return this.db.dropCollection(table);
 	}
 
 
 	public getAll(table: string, filter = []): Promise<any[]> {
-		// @ts-ignore
 		if (filter.length) return this.db.collection(table).find({ id: { $in: filter } }, { _id: 0 }).toArray();
-		// @ts-ignore
 		return this.db.collection(table).find({}, { _id: 0 }).toArray();
 	}
 
 	public getKeys(table: string): Promise<any> {
-		// @ts-ignore
 		return this.db.collection(table).find({}, { id: 1, _id: 0 }).toArray();
 	}
 
 	public get(table: string, id: string): Promise<any> {
-		// @ts-ignore
 		return this.db.collection(table).findOne(resolveQuery(id));
 	}
 
 	public has(table: string, id: string): Promise<boolean> {
-		// @ts-ignore
 		return this.get(table, id).then(Boolean);
 	}
 
 	public getRandom(table: string): Promise<any> {
-		// @ts-ignore
 		return this.db.collection(table).aggregate({ $sample: { size: 1 } });
 	}
 
 	public create(table: string, id: string, doc: object = {}): Promise<any> {
-		// @ts-ignore
 		return this.db.collection(table).insertOne(util.mergeObjects(this.parseUpdateInput(doc), resolveQuery(id)));
 	}
 
 	public delete(table: string, id: string): Promise<any> {
-		// @ts-ignore
 		return this.db.collection(table).deleteOne(resolveQuery(id));
 	}
 
 	public update(table: string, id: string, doc: any): Promise<any> {
-		// @ts-ignore
-		return this.db.collection(table).updateOne(resolveQuery(id), { $set: isObject(doc) ? flatten(doc) : parseEngineInput(doc) });
+		return this.db.collection(table).updateOne(resolveQuery(id), { $set: util.isObject(doc) ? flatten(doc) : parseEngineInput(doc) });
 	}
 
 	public replace(table: string, id: string, doc: any): Promise<any> {
-		// @ts-ignore
 		return this.db.collection(table).replaceOne(resolveQuery(id), this.parseUpdateInput(doc));
 	}
 
 }
 
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const resolveQuery = query => util.isObject(query) ? query : { id: query };
 
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 function flatten(obj, path = '') {
 	let output = {};
 	for (const [key, value] of Object.entries(obj)) {
@@ -109,6 +98,7 @@ function flatten(obj, path = '') {
 	return output;
 }
 
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 function parseEngineInput(updated) {
 	return Object.assign({}, ...updated.map(entry => ({ [entry.data[0]]: entry.data[1] })));
 }
